44116b2b748d9fe86529a3aac20e05368bcafceb
Lucas Juste <lu.scatollini@hotmail.com>
2021-12-09 23:28:16 -0300

    newww

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,4 @@
 import type { NextPage } from 'next';
-import FormView from '../components/FormView';
-import HomeScreen from '../components/HomeScreen';
-import ModernServices from '../components/ModernServices';
-import WhoIsLuana from '../components/WhoIsLuana';
-import Header from '../shared/components/Header';
-import Footer from '../shared/Footer';
 
 import { Flex, Text, Heading } from '@chakra-ui/react';
 import Image from 'next/image';
