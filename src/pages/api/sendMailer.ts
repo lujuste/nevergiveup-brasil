68ef8c8f96865251bea4f9bd11716c14dbd793e0
Lucas Juste <lu.scatollini@hotmail.com>
2021-12-14 01:54:33 -0300

    Neewww

--- src/pages/api/sendMailer.ts
+++ src/pages/api/sendMailer.ts
@@ -1,7 +1,10 @@
 import nodemailer from 'nodemailer';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function sendMailer(req: NextApiRequest, res: NextApiResponse) {
+export default async function sendMailer(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   let transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
@@ -12,26 +15,29 @@ export default function sendMailer(req: NextApiRequest, res: NextApiResponse) {
     },
   });
 
-  transporter
-    .sendMail({
-      from: process.env.USERMAIL,
-      to: req.body.email,
-      cc: 'lucianafeldman@gmail.com',
-      subject: 'Inscrição confirmada!!!!',
-      replyTo: process.env.USERMAIL,
-      text: 'Participe do curso de mulheres na política!',
-      html: `<h1>Parabéns! Sua inscrição foi efetivada com sucesso!</h1> <p> Prezada ${req.body.name},<br/> Você concluiu a sua inscrição no processo de seleção da primeira
-      turma da <strong>CONECTA.</strong> <br/> Nosso time fará uma avaliação
-      dos critérios de participação e entrará em contato com você até
-      <strong>
-       
-        11/01/2022 para confirmar a sua participação. <br />
-      </strong> </p>`,
-    })
-    .then(response => {
-      res.send(response);
-    })
-    .catch(error => {
-      res.send(error);
-    });
+  const mailOption = {
+    from: process.env.USERMAIL,
+    to: req.body.email,
+    cc: 'lucianafeldman@gmail.com',
+    subject: 'Inscrição confirmada!!!!',
+    replyTo: process.env.USERMAIL,
+    text: 'Participe do curso de mulheres na política!',
+    html: `<h1>Parabéns! Sua inscrição foi efetivada com sucesso!</h1> <p> Prezada ${req.body.name},<br/> Você concluiu a sua inscrição no processo de seleção da primeira
+    turma da <strong>CONECTA.</strong> <br/> Nosso time fará uma avaliação
+    dos critérios de participação e entrará em contato com você até
+    <strong>
+     
+      11/01/2022 para confirmar a sua participação. <br />
+    </strong> </p>`,
+  };
+
+  await transporter.sendMail(mailOption, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.send('error' + JSON.stringify(err));
+    } else {
+      console.log('mail send');
+      res.send('success');
+    }
+  });
 }
